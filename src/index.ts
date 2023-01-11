1e17accdaf5e4c2475e3b7b77cf63df9bb7dce6b
Margo <margokomilfo.dev@gmail.com>
2023-01-11 22:57:27 +0100

    sprint 1 - 1 (added main GET =))

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, {Request, Response} from 'express'
 import bodyParser from 'body-parser'
 import {addressesRouter} from "./routes/addresses-router";
 import {productsRouter} from "./routes/products-router";
@@ -10,6 +10,9 @@ app.use(bodyParser({}))
 app.use('/addresses', addressesRouter)
 app.use('/products', productsRouter)
 
+app.get('/', (req: Request, res: Response)=> {
+    res.send('Hello back-end lessons in it-incubator!!!')
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
